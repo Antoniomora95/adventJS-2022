7adf081c831bcc09f99a2f991977723ae23b5ca5
Antonio Mora <antonio.mora@oracle.com>
2022-12-21 17:11:24 -0600

    add gift-wrap algorithm

---
+++ gift-wrapping.js
@@ -0,0 +1,16 @@
+// https://adventjs.dev/en/challenges/2022/1
+
+function wrapping(gifts) {
+    return gifts.map(gft => {
+      let symbol_ = '*';
+      let giftWithSymbol = `${symbol_}${gft}${symbol_}`;
+      let repeatSymbol = symbol_.repeat(gft.length+2);
+      let res = `${repeatSymbol}\n${giftWithSymbol}\n${repeatSymbol}`;
+      return res;
+    });
+}
+
+const gifts = ['cat', 'game', 'socks']
+const wrapped = wrapping(gifts)
+
+console.log(wrapped)
