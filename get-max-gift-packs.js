eff2fcaf97aa13fe80fe58da010ca20208c6391c
Antonio Mora Bautista <morabautistaantonio@gmail.com>
2022-12-27 20:42:10 -0600

    Refactor get-max-gift-packs, increase score, op/s, and decrease cognitive complexity

--- get-max-gift-packs.js
+++ get-max-gift-packs.js
@@ -1,16 +1,11 @@
 // https://adventjs.dev/en/challenges/2022/3
+
 function distributeGifts(packOfGifts, reindeers) {
-    const packWeightFn = (totalWeight, weight) => totalWeight + weight.length;
-    const reindeersCapacityFn = (capacity, reindeer) => ((reindeer.length * 2) + capacity);
-  
-    const packWeight = packOfGifts.reduce(packWeightFn, 0);
-    const reeindersCapacity = reindeers.reduce(reindeersCapacityFn, 0);
-    return Math.floor(reeindersCapacity / packWeight);
-  }
+  const w = packOfGifts.join('').length;
+  const c = reindeers.join('').length * 2;
+  return Math.floor(c/w);
+}
+
 
 const packOfGifts = ["book", "doll", "ball"]
 const reindeers = ["dasher", "dancer"]
-
-// pack weights 4 + 4 + 4 = 12
-// reindeers can carry (2 * 6) + (2 * 6) = 24
-console.log(distributeGifts(packOfGifts, reindeers)); // 2
